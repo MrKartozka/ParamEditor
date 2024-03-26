3085f267fc8ec22c1766edebf2dddb6af4a94709
MrKartozka <mrkartozka@mail.ru>
2024-03-26 16:40:54 +0300

    ParamEditor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+interface Param {
+  id: number;
+  name: string;
+  type: string;
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+interface State {
+  model: Model;
+}
+
+class ParamEditor extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      model: this.props.model,
+    };
+  }
+
+  updateParamValue = (paramId: number, value: string) => {
+    const newModel = {
+      ...this.state.model,
+      paramValues: this.state.model.paramValues.map(paramValue =>
+        paramValue.paramId === paramId
+          ? { ...paramValue, value }
+          : paramValue
+      ),
+    };
+    this.setState({ model: newModel });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.params.map(param => (
+          <div key={param.id}>
+            <label>
+              {param.name}:
+              <input
+                type="text"
+                value={
+                  this.state.model.paramValues.find(pv => pv.paramId === param.id)?.value || ''
+                }
+                onChange={e =>
+                  this.updateParamValue(param.id, e.target.value)
+                }
+              />
+            </label>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+const params = [
+  { id: 1, name: 'Назначение', type: 'string' },
+  { id: 2, name: 'Длина', type: 'string' },
+];
+
+const model = {
+  paramValues: [
+    { paramId: 1, value: 'посвященное' },
+    { paramId: 2, value: 'макси' },
+  ],
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParamEditor params={params} model={model} />
     </div>
   );
 }
